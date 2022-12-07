4709bbc8cf73a0c22f5d01c399d17aa76656f1e9
Attache RnD <attachee@eclectics.io>
2022-12-07 16:25:37 +0300

    wrote more code and debugging

--- app/src/main/java/com/steve_md/joomia/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/steve_md/joomia/ui/fragments/HomeFragment.kt
@@ -7,14 +7,3 @@ import android.view.View
 import android.view.ViewGroup
 import com.steve_md.joomia.R
 
-class HomeFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-}
