d340e669e613ad7f1d25246c6174eacda68bdc8b
MuindiStephen <stephenmuindi241@gmail.com>
2022-12-16 11:22:41 +0300

    worked on cart

--- app/src/main/java/com/steve_md/joomia/adapters/CartAdapter.kt
+++ app/src/main/java/com/steve_md/joomia/adapters/CartAdapter.kt
@@ -1,12 +1,16 @@
 package com.steve_md.joomia.adapters
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.steve_md.joomia.data.local.entity.CartEntity
+import com.steve_md.joomia.databinding.CartItemRowBinding
 
 class CartAdapter (
     private val onClickListener: OnClickListener
@@ -23,16 +27,30 @@ class CartAdapter (
 
     }
 
-    inner class CartViewHolder(itemView: View) :  RecyclerView.ViewHolder(itemView) {
+    inner class CartViewHolder(private val binding:CartItemRowBinding) :  RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(cartLineItem: CartEntity?) {
+            Glide.with(binding.imageView3).load(cartLineItem?.image)
+                .centerCrop()
+                .fitCenter()
+                .into(binding.imageView3)
+
+            binding.textViewProductName.text = cartLineItem?.title
+            binding.textView2.text = "$"+cartLineItem?.price.toString()
+        }
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
-        TODO("Not yet implemented")
+        return CartViewHolder(
+            CartItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val cartLineItem = getItem(position)
+        holder.bind(cartLineItem)
     }
 
 }
