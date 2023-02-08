690dda63b5504a940f388e9ca4fd8acca07efa4d
MuindiStephen <stephenmuindi241@gmail.com>
2023-02-08 16:49:23 +0300

    splash/code refactors

--- app/src/main/java/com/steve_md/joomia/ui/fragments/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/steve_md/joomia/ui/fragments/splash/SplashScreenFragment.kt
@@ -3,6 +3,7 @@ package com.steve_md.joomia.ui.fragments.splash
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.LottieDrawable
 import com.steve_md.joomia.R
 import com.steve_md.joomia.databinding.FragmentSplashScreenBinding
 import com.steve_md.joomia.util.ApiStates
@@ -21,29 +24,33 @@ class SplashScreenFragment : Fragment() {
 
     // binding
     private lateinit var binding: FragmentSplashScreenBinding
-    // private val viewModel : SplashViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
 
        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
 
-//        viewModel._liveData.observe(viewLifecycleOwner) {
-//            findNavController().navigate(R.id.action_splashScreenFragment_to_mainActivity)
-//        }
-//        viewModel.setValue()
-
-
-        Handler().postDelayed(
+        Handler(Looper.getMainLooper()).postDelayed(
             {
             findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
-        }, 3000
-        )
+        }, 3000)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpAnimation()
+    }
+
+    private fun setUpAnimation() {
+        binding.apply {
+            splashScreenLottieAnimation.setAnimation(R.raw.animation)
+            splashScreenLottieAnimation.repeatCount = LottieDrawable.INFINITE
+            splashScreenLottieAnimation.playAnimation()
+        }
     }
 
 }
