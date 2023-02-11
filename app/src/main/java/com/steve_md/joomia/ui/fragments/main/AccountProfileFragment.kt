b2825d7676d5a8667dc92a48732c1b13092e4c57
MuindiStephen <stephenmuindi241@gmail.com>
2023-02-11 16:14:30 +0300

    chore: done user-profile

--- app/src/main/java/com/steve_md/joomia/ui/fragments/main/AccountProfileFragment.kt
+++ app/src/main/java/com/steve_md/joomia/ui/fragments/main/AccountProfileFragment.kt
@@ -16,7 +16,8 @@ class AccountProfileFragment : Fragment() {
     private lateinit var binding : FragmentAccountProfileBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentAccountProfileBinding.inflate(
