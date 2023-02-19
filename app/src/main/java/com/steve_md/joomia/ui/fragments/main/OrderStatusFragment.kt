10979e60f4181a27d1db92a30af4decf7bbd5b25
MuindiStephen <stephenmuindi241@gmail.com>
2023-02-19 19:38:08 +0300

    more code implementation

---
+++ app/src/main/java/com/steve_md/joomia/ui/fragments/main/OrderStatusFragment.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c)  Stephen Muindi
+ */
+
+package com.steve_md.joomia.ui.fragments.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.steve_md.joomia.R
+import com.steve_md.joomia.databinding.FragmentOrderStatusBinding
+
+class OrderStatusFragment : Fragment() {
+
+    private lateinit var binding: FragmentOrderStatusBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentOrderStatusBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
