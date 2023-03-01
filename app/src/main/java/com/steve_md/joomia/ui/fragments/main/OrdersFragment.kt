58f62257f57b0090765e7adc1bac5489d3d275eb
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-01 18:45:28 +0300

    code-refactoring

--- app/src/main/java/com/steve_md/joomia/ui/fragments/main/OrdersFragment.kt
+++ app/src/main/java/com/steve_md/joomia/ui/fragments/main/OrdersFragment.kt
@@ -5,11 +5,10 @@
 package com.steve_md.joomia.ui.fragments.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.steve_md.joomia.R
+import androidx.fragment.app.Fragment
 import com.steve_md.joomia.databinding.FragmentOrdersBinding
 
 
@@ -19,7 +18,7 @@ class OrdersFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentOrdersBinding.inflate(
             inflater,
@@ -32,8 +31,6 @@ class OrdersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
     }
 
 
