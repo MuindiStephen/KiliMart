f92cad11e9180f29ad98d6dfb28890cceac08d70
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-10 11:52:47 +0300

    paypal design (#28)

--- app/src/main/java/com/steve_md/joomia/ui/fragments/main/PaypalFragment.kt
+++ app/src/main/java/com/steve_md/joomia/ui/fragments/main/PaypalFragment.kt
@@ -5,34 +5,22 @@
 package com.steve_md.joomia.ui.fragments.main
 
 import android.os.Bundle
-<<<<<<< HEAD
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.steve_md.joomia.databinding.FragmentPaypalBinding
 
 class PaypalFragment : Fragment() {
 
     private lateinit var binding: FragmentPaypalBinding
 
-=======
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.steve_md.joomia.R
-
-
-class PaypalFragment : Fragment() {
-
->>>>>>> 26-paypal-payments-integration
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-<<<<<<< HEAD
         binding = FragmentPaypalBinding.inflate(inflater,container,false)
         return binding.root
     }
@@ -43,11 +31,6 @@ class PaypalFragment : Fragment() {
     }
 
     private fun setUpBinding() {
-        TODO("Not yet implemented")
+        binding.imageView4.setOnClickListener { findNavController().navigateUp() }
     }
-=======
-        return inflater.inflate(R.layout.fragment_paypal, container, false)
-    }
-
->>>>>>> 26-paypal-payments-integration
 }
